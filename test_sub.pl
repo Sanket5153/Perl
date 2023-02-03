0534244286ef3fbfe05ef83b0fcb71f9bce1dc5c
Sanket5153 <118500008+Sanket5153@users.noreply.github.com>
2023-02-03 13:01:31 +0530

    Add files via upload

---
+++ test_sub.pl
@@ -0,0 +1,12 @@
+package sub;
+
+sub sub_fun{
+	print("EnTER NUM1 : ");
+	$num1 =<>;
+	print("ENter num2 : ");
+	$num2=<>;
+	$sub = $num1-$num2;
+	print("sub : $sub\n");
+}
+
+return 1;
---
+++ test_sum.pl
@@ -0,0 +1,12 @@
+package sum;
+
+sub sum_fun{
+	print("EnTER NUM1 : ");
+	$num1 =<>;
+	print("ENter num2 : ");
+	$num2=<>;
+	$sum = $num1+$num2;
+	print("sum : $sum\n");
+}
+
+return 1;
